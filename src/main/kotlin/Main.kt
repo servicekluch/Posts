0b915ead6e30cf8ce034d4a0537375a35e7ed8b6
servicekluch <samara04@yandex.ru>
2022-03-18 09:19:03 +0400

    changing the variable to camelCase

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,21 +14,21 @@ fun main() {
 data class Post(
     val id: Int = 0,
     /*Идентификатор записи*/
-    val owner_id: Int = 0,
+    val ownerId: Int = 0,
     /*Идентификатор владельца стены, на которой размещена запись. В версиях API ниже 5.7 это поле называется to_id.*/
-    val from_id: Int = 0,
+    val fromId: Int = 0,
     /*Идентификатор автора записи (от чьего имени опубликована запись).*/
-    val created_by: Int = 0,
+    val createdBy: Int = 0,
     /*Идентификатор администратора, который опубликовал запись (возвращается только для сообществ при запросе с ключом доступа администратора). Возвращается в записях, опубликованных менее 24 часов назад.*/
     val date: Int = 0,
     /*Время публикации записи в формате unixtime.*/
     val text: String = "",
     /*Текст записи.*/
-    val reply_owner_id: Int = 0,
+    val replyOwnerId: Int = 0,
     /*Идентификатор владельца записи, в ответ на которую была оставлена текущая.*/
-    val reply_post_id: Int = 0,
+    val replyPostId: Int = 0,
     /*Идентификатор записи, в ответ на которую была оставлена текущая.*/
-    val friends_only: Boolean = false,
+    val friendsOnly: Boolean = false,
     /*1, если запись была создана с опцией «Только для друзей».*/
     val comments: Comments? = null,
     /*Информация о комментариях к записи*/
@@ -40,25 +40,25 @@ data class Post(
     /*Информация о репостах записи («Рассказать друзьям»)*/
     val views: Views? = null,
     /*Информация о просмотрах записи*/
-    val post_type: String = "",
+    val postType: String = "",
     /*Тип записи, может принимать следующие значения: post, copy, reply, postpone, suggest.*/
-    val signer_id: Int = 0,
+    val signerId: Int = 0,
     /*Идентификатор автора, если запись была опубликована от имени сообщества и подписана пользователем;*/
-    val can_pin: Boolean = false,
+    val canPin: Boolean = false,
     /*Информация о том, может ли текущий пользователь закрепить запись (1 — может, 0 — не может).*/
-    val can_delete: Boolean = false,
+    val canDelete: Boolean = false,
     /*Информация о том, может ли текущий пользователь удалить запись (1 — может, 0 — не может).*/
-    val can_edit: Boolean = false,
+    val canEdit: Boolean = false,
     /*Информация о том, может ли текущий пользователь редактировать запись (1 — может, 0 — не может).*/
-    val is_pinned: Boolean = false,
+    val isPinned: Boolean = false,
     /*Информация о том, что запись закреплена.*/
-    val marked_as_ads: Boolean = false,
+    val markedAsAds: Boolean = false,
     /*Информация о том, содержит ли запись отметку «реклама» (1 — да, 0 — нет).*/
-    val is_favorite: Boolean = false,
+    val isFavorite: Boolean = false,
     /*true, если объект добавлен в закладки у текущего пользователя.*/
     val donut: Donut? = null,
     /*Информация о записи VK Donut:*/
-    val postponed_id: Int = 0
+    val postponedId: Int = 0
     /*Идентификатор отложенной записи. Это поле возвращается тогда, когда запись стояла на таймере.*/
 ) {
     override fun toString(): String {
@@ -67,13 +67,13 @@ data class Post(
 }
 data class Comments (
     /*количество комментариев;*/
-    val can_post: Boolean = false,
+    val canPost: Boolean = false,
     /*информация о том, может ли текущий пользователь комментировать запись (1 — может, 0 — не может);*/
-    val groups_can_post: Boolean = false,
+    val groupsCanPost: Boolean = false,
     /*информация о том, могут ли сообщества комментировать запись;*/
-    val can_close: Boolean = false,
+    val canClose: Boolean = false,
     /*может ли текущий пользователь закрыть комментарии к записи;*/
-    val can_open: Boolean = false
+    val canOpen: Boolean = false
     /*может ли текущий пользователь открыть комментарии к записи.*/
 )
 
@@ -88,11 +88,11 @@ class Likes (
    /*Информация о лайках к записи*/
    val count: Int = 0,
    /*число пользователей, которым понравилась запись;*/
-   val user_likes: Boolean = false,
+   val userLikes: Boolean = false,
    /*наличие отметки «Мне нравится» от текущего пользователя (1 — есть, 0 — нет);*/
-   val can_like: Boolean = false,
+   val canLike: Boolean = false,
    /*информация о том, может ли текущий пользователь поставить отметку «Мне нравится» (1 — может, 0 — не может);*/
-   val can_publish: Boolean = false
+   val canPublish: Boolean = false
    /*информация о том, может ли текущий пользователь сделать репост записи (1 — может, 0 — не может).*/
 )
 
@@ -100,7 +100,7 @@ class Reports (
     /*Информация о репостах записи («Рассказать друзьям»)*/
     val count: Int = 0,
     /*число пользователей, скопировавших запись;*/
-    val user_reposted: Boolean = false
+    val userReposted: Boolean = false
     /*наличие репоста от текущего пользователя (1 — есть, 0 — нет).*/
 )
 
@@ -112,15 +112,15 @@ class Views (
 
 data class Donut (
    /*Информация о записи VK Donut:*/
-   val is_donut: Boolean = false,
+   val isDonut: Boolean = false,
    /*запись доступна только платным подписчикам VK Donut;*/
-   val paid_duration: Int = 0,
+   val paidDuration: Int = 0,
    /*время, в течение которого запись будет доступна только платным подписчикам VK Donut;*/
    val placeholder: Placeholder? = null,
    /*заглушка для пользователей, которые не оформили подписку VK Donut. Отображается вместо содержимого записи.*/
-   val can_publish_free_copy: Boolean = false,
+   val canPublishFreeCopy: Boolean = false,
    /*можно ли открыть запись для всех пользователей, а не только подписчиков VK Donut;*/
-   val edit_mode: EditMode = EditMode.all
+   val editMode: EditMode = EditMode.all
    /*информация о том, какие значения VK Donut можно изменить в записи*/
 )
 
