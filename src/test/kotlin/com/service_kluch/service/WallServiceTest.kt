eefeded2ef0486f9b364f0d65ec5aad5df7b52c4
servicekluch <samara04@yandex.ru>
2022-03-23 12:24:42 +0400

    WallServiceTest in package

---
+++ src/test/kotlin/com/service_kluch/service/WallServiceTest.kt
@@ -0,0 +1,16 @@
+package com.service_kluch.service
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WallServiceTest {
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun update() {
+    }
+}
