4d975c11167bfbefea58ff402fad10d42779f0e1
servicekluch <samara04@yandex.ru>
2022-03-30 12:04:53 +0400

    add PostNotFoundException

--- src/test/kotlin/com/service_kluch/WallServiceTest.kt
+++ src/test/kotlin/com/service_kluch/WallServiceTest.kt
@@ -1,5 +1,9 @@
-package com.service_kluch
+package com.service_kluch.service
 
+import com.service_kluch.Comment
+import com.service_kluch.Post
+import com.service_kluch.PostNotFoundException
+import com.service_kluch.WallService
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -26,6 +30,28 @@ class WallServiceTest {
         val wallService = WallService()
         val firstPost = wallService.add(Post(text="Hello VK on Java", date = 2022))
         val result = wallService.update(firstPost.copy(text = "Hello VK on Kotlin"))
+        assertFalse(result)
+    }
+
+    @Test (expected = PostNotFoundException::class)
+    fun createComment() {
+        val wallService = WallService()
+        wallService.createComment(Comment(3, 5))
+        wallService.add(Post(3))
+        val create = Comment(7, 5)
+        val result = wallService.createComment(create)
+
+        assertTrue(result)
+    }
+
+    @Test(expected = PostNotFoundException::class)
+    fun shouIdTrow(){
+        val wallService = WallService()
+        wallService.createComment(Comment(3, 5))
+        wallService.add(Post(3))
+        val create = Comment(7, 15)
+        val result = wallService.createComment(create)
+
         assertFalse(result)
     }
 }
