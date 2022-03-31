57b2219ef94d3b2ac5c00163d45f4241890b675f
servicekluch <97802322+servicekluch@users.noreply.github.com>
2022-03-31 10:11:26 +0400

    Update Attachment.kt

--- src/main/kotlin/com/service_kluch/objects/attachments/Attachment.kt
+++ src/main/kotlin/com/service_kluch/objects/attachments/Attachment.kt
@@ -5,24 +5,24 @@ interface Attachment {
 }
 
 enum class AttachmentTypeEnum {
-    /*Фотография (type = photo)
-    Видеозапись (type = video)
-    Аудиозапись (type = audio)
-    Документ (type = doc)
-    Граффити (type = graffiti)
-    Ссылка (type = link)
-    Заметка (type = note)
-    Контент приложения (type = app)
-    Опрос (type = poll)
-    Вики-страница (type = page)
-    Альбом с фотографиями (type=album)
-    Список фотографий (type=photos_list)
-    Товар (type = market)
-    Подборка товаров (type = market_album)
-    Стикер (type = sticker)
-    Карточки (type = pretty_cards)
-    Встреча (type = event)*/
-
+    /**Фотография (type = photo)
+    * Видеозапись (type = video)
+    * Аудиозапись (type = audio)
+    * Документ (type = doc)
+    * Граффити (type = graffiti)
+    * Ссылка (type = link)
+    * Заметка (type = note)
+    * Контент приложения (type = app)
+    * Опрос (type = poll)
+    * Вики-страница (type = page)
+    * Альбом с фотографиями (type=album)
+    * Список фотографий (type=photos_list)
+    * Товар (type = market)
+    * Подборка товаров (type = market_album)
+    * Стикер (type = sticker)
+    * Карточки (type = pretty_cards)
+    * Встреча (type = event)
+    */
     PHOTO, VIDEO, AUDIO, DOC, GRAFFITI,
     LINK, NOTE, APP, POLL, PAGE,
     ALBUM, PHOTOS_LIST, MARKET, MARKET_ALBUM,
